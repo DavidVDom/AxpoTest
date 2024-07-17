3bd839035e3f3d9aeda5bd8b894b87fc3b4fa50a
DavidAsmodeus <david.threshold@gmail.com>
2024-07-17 22:27:38 +0200

    Add assembly and create worker service

--- AxpoTest/Program.cs
+++ AxpoTest/Program.cs
@@ -1,6 +1,8 @@
+using Axpo;
 using AxpoTest;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddTransient<IPowerService, PowerService>();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
--- AxpoTest/Worker.cs
+++ AxpoTest/Worker.cs
@@ -1,23 +1,29 @@
+using Axpo;
+
 namespace AxpoTest
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IPowerService _powerService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IPowerService powerService)
         {
             _logger = logger;
+            _powerService = powerService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                //if (_logger.IsEnabled(LogLevel.Information))
+                //{
+                //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //}
+                //await Task.Delay(1000, stoppingToken);
+
+                var algo = await _powerService.GetTradesAsync(DateTime.Now);
             }
         }
     }
